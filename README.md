08f5c3fbc641465190150c525f935a183aa1396c
Raja Yaser Hussain <yaser@Yasers-MBP.fios-router.home>
2024-01-03 15:51:55 -0500

    Final changes to javascript code and css styling; finished readme file; added screenshot of live application to images folder

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # weather-dashboard
 Week 6 Challenge
+
+In this challenge, I was given the task of creating a weather forecast application. This application contains the following features:
+
+- search functionality for any given city in the world
+
+- weather data (temperature, wind speed, and humidity) for the current day and the next 5 consecutive days
+
+- search history with button to clear history
+
+
+## User Story
+AS A traveler
+I WANT to see the weather outlook for multiple cities
+SO THAT I can plan a trip accordingly
+
+## Acceptance Criteria
+GIVEN a weather dashboard with form inputs
+WHEN I search for a city
+THEN I am presented with current and future conditions for that city and that city is added to the search history
+WHEN I view current weather conditions for that city
+THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+WHEN I view future weather conditions for that city
+THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+WHEN I click on a city in the search history
+THEN I am again presented with current and future conditions for that city
+
+
+Live application: https://yaserhdev.github.io/weather-dashboard/
+
+![Screenshot of deployed application](assets/images/weather-dashboard-screenshot.png)
+
+* Source code: yaser/bootcamp/weather-dashboard
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var cities = JSON.parse(localStorage.getItem("city")) || [];
 
 // Function to retrieve and display weather data
 function getForecast(cityName) {
-
+    // Code for first API call which retrieves and displays weather data for current day
     var todayURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=' + apiKey;
     $.ajax({
         url: todayURL,
@@ -13,23 +13,23 @@ function getForecast(cityName) {
     })
     .then(function(response) {
         var inputCity = response.name;
-        saveHistory(inputCity);
         var currentIcon = response.weather[0].icon;
         var currentIconURL = "http://openweathermap.org/img/w/" + currentIcon + ".png";
         var currentTemp = parseInt(response.main.temp) + " °F";
         var currentWind = parseInt(response.wind.speed) + "mph";
         var currentHumidity = parseInt(response.main.humidity) + "%";
-        console.log(response);
         $("#city").text(inputCity);
         $("#icon-today").attr({"src": currentIconURL});
         $("#temp-today").text(currentTemp);
         $("#wind-today").text(currentWind);
         $("#humidity-today").text(currentHumidity);
+        saveHistory(inputCity);
     }) 
+    // Code for alert message when invalid city is entered
     .catch(function(e) {
         alert("Please enter a valid city name!");
     });
-
+    // Code for second API call which retrieves and displays weather data for next 5 consecutive days
     var forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&units=imperial&appid=' + apiKey;
     $.ajax({
         url: forecastURL,
@@ -41,11 +41,10 @@ function getForecast(cityName) {
         var dayOneTemp = parseInt(response.list[3].main.temp) + " °F";
         var dayOneWind = parseInt(response.list[3].wind.speed) + "mph";
         var dayOneHumidity = parseInt(response.list[3].main.humidity) + "%";
-        console.log(response);
         $("#icon-day-one").attr({"src": dayOneIconURL});
         $("#temp-day-one").text(dayOneTemp);
         $("#wind-day-one").text(dayOneWind);
-        $("#humidity-day-one").text(dayOneHumidity.trim());
+        $("#humidity-day-one").text(dayOneHumidity);
 
         var dayTwoIcon = response.list[11].weather[0].icon;
         var dayTwoIconURL = "http://openweathermap.org/img/w/" + dayTwoIcon + ".png";
@@ -55,7 +54,7 @@ function getForecast(cityName) {
         $("#icon-day-two").attr({"src": dayTwoIconURL});
         $("#temp-day-two").text(dayTwoTemp);
         $("#wind-day-two").text(dayTwoWind);
-        $("#humidity-day-two").text(dayTwoHumidity.trim());
+        $("#humidity-day-two").text(dayTwoHumidity);
 
         var dayThreeIcon = response.list[19].weather[0].icon;
         var dayThreeIconURL = "http://openweathermap.org/img/w/" + dayThreeIcon + ".png";
@@ -65,7 +64,7 @@ function getForecast(cityName) {
         $("#icon-day-three").attr({"src": dayThreeIconURL});
         $("#temp-day-three").text(dayThreeTemp);
         $("#wind-day-three").text(dayThreeWind);
-        $("#humidity-day-three").text(dayThreeHumidity.trim());
+        $("#humidity-day-three").text(dayThreeHumidity);
 
         var dayFourIcon = response.list[27].weather[0].icon;
         var dayFourIconURL = "http://openweathermap.org/img/w/" + dayFourIcon + ".png";
@@ -75,7 +74,7 @@ function getForecast(cityName) {
         $("#icon-day-four").attr({"src": dayFourIconURL});
         $("#temp-day-four").text(dayFourTemp);
         $("#wind-day-four").text(dayFourWind);
-        $("#humidity-day-four").text(dayFourHumidity.trim());
+        $("#humidity-day-four").text(dayFourHumidity);
 
         var dayFiveIcon = response.list[35].weather[0].icon;
         var dayFiveIconURL = "http://openweathermap.org/img/w/" + dayFiveIcon + ".png";
@@ -85,9 +84,9 @@ function getForecast(cityName) {
         $("#icon-day-five").attr({"src": dayFiveIconURL});
         $("#temp-day-five").text(dayFiveTemp);
         $("#wind-day-five").text(dayFiveWind);
-        $("#humidity-day-five").text(dayFiveHumidity.trim());
+        $("#humidity-day-five").text(dayFiveHumidity);
     });
-
+    // Code to reset search input field
     $("#city-input").val("");
 };
 
@@ -98,11 +97,13 @@ function displayHistory() {
         var h5 = document.createElement("h5");
         h5.textContent = cities[i];
         searchHistory.appendChild(h5);
+        h5.classList.add("card", "search");
     };
+    // Code for click event on search history cities
     searchHistory.addEventListener("click", populateHistory);
 };
 
-// Function to populate weather data based off search history
+// Function to populate weather data based off city from search history
 function populateHistory(event) {
     var cityName = event.target.textContent;
     getForecast(cityName);
@@ -128,6 +129,18 @@ function displayForecast() {
 // Event listener for search button
 $("#submit").on("click", displayForecast);
 
+// Event listener for clear history button
+$("#clear-history").on("click", function() {
+    $("h5").remove();
+    localStorage.clear();
+    resetPage();
+});
+
+// Function to reset page to initial state
+function resetPage() {
+    location.reload();
+};
+
 // Code to display today's date and the dates of the next 5 consecutive days
 let dateToday = new Date();
 $('#date-today').text(dateToday.toLocaleDateString());
@@ -152,4 +165,5 @@ let dayFive = new Date();
 dayFive.setDate(dayFive.getDate() + 5);
 $('#day-five').text(dayFive.toLocaleDateString());
 
+// Code to display search history on page load
 displayHistory();
